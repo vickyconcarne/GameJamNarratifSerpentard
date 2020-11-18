3c64a1c6fada480dbba3fd7c49884ccc68722e11
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2020-11-18 23:15:08 +0100

    Added manual with symbols (didnt finish scaling and animations)

--- Assets/Scripts/Misc/ManualController.cs
+++ Assets/Scripts/Misc/ManualController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class ManualController : MonoBehaviour
@@ -8,10 +10,16 @@ public class ManualController : MonoBehaviour
 
     public Transform contentGridParent;
 
+    public TextMeshProUGUI titleSymbol;
+    public Image imageSymbol;
+    public TextMeshProUGUI motsClefSymbol;
+    public TextMeshProUGUI descriptionSymbol;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ComposeInitialGrid();
+        ShowInfo(manualInfo.manuelList[0]);
     }
 
     // Update is called once per frame
@@ -19,4 +27,35 @@ public class ManualController : MonoBehaviour
     {
         
     }
+
+    public void ComposeInitialGrid()
+    {
+        for(int index = 0; index < manualInfo.manuelList.Count; index++ )
+        {
+            SymbolInfo si = manualInfo.manuelList[index];
+            GameObject tile = (GameObject)Instantiate(Resources.Load("UI/ManualSymbolElement"), contentGridParent);
+            tile.GetComponent<Image>().sprite = si.symbolIcon;
+            AddListenersToGridObject(tile, si);
+        }
+    }
+
+    public void AddListenersToGridObject(GameObject tile, SymbolInfo si)
+    {
+        //Button
+        Button b = tile.GetComponent<Button>();
+        b.onClick.AddListener(delegate { ShowInfo(si); });
+    }
+
+    public void ShowInfo(SymbolInfo si)
+    {
+        titleSymbol.text = si.symbolName;
+        imageSymbol.sprite = si.symbolIcon;
+        motsClefSymbol.text = si.motsClefs;
+        descriptionSymbol.text = si.description;
+    }
+
+
+
+
+
 }
