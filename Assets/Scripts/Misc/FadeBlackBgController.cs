784309caa5100ea10af21b1942415ea9f900a667
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2020-11-19 12:16:26 +0100

    Changed manuel animations finished fade to black

---
+++ Assets/Scripts/Misc/FadeBlackBgController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBlackBgController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeactivateObject()
+    {
+        Debug.Log("deactivated bg!");
+        this.gameObject.SetActive(false);
+    }
+
+    public void ActivateBg()
+    {
+        Debug.Log("activating bg!");
+        this.gameObject.SetActive(true);
+        this.GetComponent<Animator>().SetTrigger("FadeIn");
+    }
+
+    public void DeactivateBg()
+    {
+        this.GetComponent<Animator>().SetTrigger("FadeOut");
+    }
+}
