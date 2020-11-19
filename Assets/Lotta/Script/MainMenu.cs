3729640b3731e17831e76340ef76aadc62e3535d
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2020-11-19 10:44:39 +0100

    ajout de script menu transition

--- Assets/Lotta/Script/MainMenu.cs
+++ Assets/Lotta/Script/MainMenu.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private bool goToScene = false;
+    public Animator animatorTransitionToScene;
+    public string nameOfTrigger;
+    public float waitTime;
     public void PlayGame()
-
     {
-        SceneManager.LoadScene("GameScene");
+        if (!goToScene)
+        {
+            goToScene = true;
+            StartCoroutine("PlaySceneCoroutine");
+        }
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private IEnumerator PlaySceneCoroutine()
+    {
+        if (animatorTransitionToScene)
+        {
+            animatorTransitionToScene.SetTrigger(nameOfTrigger);
+        }
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("GameScene");
     }
 }
