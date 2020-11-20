b65f1faf5edb707dbf1ffb3a8f8a7a32477980b2
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2020-11-20 02:50:17 +0100

    finished cinematic

---
+++ Assets/BeginTransitionScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginTransitionScene : MonoBehaviour
+{
+    public Animator animationTransition;
+    public string triggerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationTransition.SetTrigger(triggerName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
